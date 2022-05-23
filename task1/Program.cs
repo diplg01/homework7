7fce94a57b6d0882bf2998eea68dbc38b09787c4
diplg <dimas4354@icloud.com>
2022-05-24 01:30:11 +0300

    задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int m = 4;
+int n = 4;
+double[,] array = new double[m, n];
+
+FillArray();
+Console.WriteLine("Дан массив: ");
+PrintArray();
+
+void FillArray()
+{
+    Random random = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = Convert.ToDouble(random.Next(-1000, 1001)) / 10;
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j] + "       ");
+        }
+        Console.WriteLine();
+    }
+}
